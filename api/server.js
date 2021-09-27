da1b87de128f29a089d38e3d9432e8bdd9dd6417
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-27 16:28:13 -0700

    complete

--- api/server.js
+++ api/server.js
@@ -85,4 +85,35 @@ server.delete('/api/users/:id', async (req, res) => {
   }
 })
 
-module.exports = server; // EXPORT YOUR SERVER instead of {}
+// .put()
+
+server.put('/api/users/:id', async (req, res) => {
+  try {
+    const specifiedUser = await User.findById(req.params.id)
+    if (!specifiedUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      })
+    } else {
+      if (!req.body.name || !req.body.bio) {
+        res.status(400).json({
+          message: 'Please provide name and bio for the user'
+        })
+      } else {
+        const updatedUser = await User.update(
+          req.params.id,
+          req.body,
+        )
+        res.status(200).json(updatedUser)
+      }
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'The user information could not be modified',
+      err: err.message,
+      stack: err.stack
+    })
+  }
+})
+
+module.exports = server;
